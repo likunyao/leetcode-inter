1e29736ec223b7dc64fe7de9a068ec3c500d0dea
leeyao <nwpulky@gmail.com>
2020-05-31 11:26:29 +0800

    695

---
+++ 695.岛屿的最大面积.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=695 lang=cpp
+ *
+ * [695] 岛屿的最大面积
+ */
+
+// @lc code=start
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxAreaOfIsland(vector<vector<int>> &grid)
+    {
+        int res = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    res = max(res, dfs(grid, i, j));
+                }
+            }
+        }
+        return res;
+    }
+    int dfs(vector<vector<int>> &grid, int i, int j)
+    {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != 1)
+            return 0;
+        grid[i][j] = -1;
+        return 1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1);
+    }
+};
+// @lc code=end
